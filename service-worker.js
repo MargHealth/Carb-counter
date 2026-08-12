878cf09355a49f31e93da3008e0f3ba3d7b35ad3
MargHealth <churchillrn@gmail.com>
2026-08-12 16:46:46 -0400

    Update cache version from v2 to v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='carb-fasting-v2',A=['./','./index.html','./manifest.json'];
+const C='carb-fasting-v3',A=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
